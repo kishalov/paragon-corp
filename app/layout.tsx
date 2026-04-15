75b3d109f332c80e58db0bb6089e61a7190084d9
kishalov <122756429+kishalov@users.noreply.github.com>
2026-04-15 15:55:40 +0000

    favicon meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
+import { Metadata } from 'next'
 import { Source_Serif_4, Source_Sans_3 } from 'next/font/google';
 import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Paragon Corp | Aviation Parts Supply | Your fearless partner for uninterrupted flights',
+  description: 'Welcome to Paragon Corp, where we elevate your aviation journey with precision and dedication. As your fearless partner in navigating the complexities of aircraft parts supply, we ensure your luxury flights remain uninterrupted. Our commitment goes beyond just supplying aviation parts; we are devoted to achieving excellence through exceptional quality and unmatched service.',
+}
 
 const sourceSerif = Source_Serif_4({
   subsets: ['latin', 'cyrillic'],
   variable: '--font-serif-pro',
   weight: ['700'],
 });
-
 
 const sourceSans = Source_Sans_3({
   subsets: ['latin', 'cyrillic'],
@@ -20,7 +25,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    
     <html lang="en" className={`${sourceSerif.variable} ${sourceSans.variable}`}>
       <body className="antialiased overflow-x-hidden">
         {children}
